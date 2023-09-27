3f5f8d180d232165f586a2887caf313c54fd3fe9
de-shyt <de.shut@inbox.ru>
2023-09-27 23:09:38 +0200

    fix date of last update

--- utils/last_update.sh
+++ utils/last_update.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-semester_dir="$1"
-pdf_dir="$semester_dir/pdf"
+cur_dir="$1"
+pdf_dir="$cur_dir/pdf"
 
 echo -n "" > "$pdf_dir/README.md"
 
 for pdf_file in "$pdf_dir"/*.pdf; do
-    pdf_name=$(basename "$pdf_file")
-    last_modified=$(date -r "$pdf_file" "+%Y-%m-%d %H:%M:%S")
-    echo "* [$pdf_name](./$pdf_name) [last update: $last_modified]" >> "$pdf_dir/README.md"
+    pdf_name=$(basename "$pdf_file" .pdf)
+    pdf_name_full="$pdf_name.pdf"
+    last_modified=$(date -r "$cur_dir/$pdf_name.md" "+%Y-%m-%d %H:%M:%S")
+    echo "* [$pdf_name](./$pdf_name_full) [last update: $last_modified]" >> "$pdf_dir/README.md"
 done
